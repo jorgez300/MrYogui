cbde5a60c7306f7244569f4cc87d38e58e3fa620
JorgeGonzalez <ing.jorge.gonzalez92@gmail.com>
2025-07-10 11:15:53 -0400

    dv d

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -12,10 +11,8 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
-  },
-  output: 'standalone'
+  serverExternalPackages: ["@prisma/client", "prisma"],
+  output: "standalone",
 };
 
 export default nextConfig;
